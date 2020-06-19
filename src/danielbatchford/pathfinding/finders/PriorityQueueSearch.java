9777fa680787c5a7eda2293240cc7334a13eb9c2
Daniel Batchford <danielbatchford@gmail.com>
2020-06-19 01:44:44 +0100

    "Fixed A Star"

--- src/danielbatchford/pathfinding/finders/PriorityQueueSearch.java
+++ src/danielbatchford/pathfinding/finders/PriorityQueueSearch.java
@@ -51,7 +51,7 @@ class PriorityQueueSearch extends PathFinder {
                 if ((workingBox.getG() + getDistance(workingBox, n)) < n.getG() || !openList.contains(n)) {
 
                     n.setG(workingBox.getG() + getDistance(workingBox, n));
-                    n.setF(n.getG() + ((useHeuristic) ? calculate(workingBox, n) : 0));
+                    n.setF(n.getG() + ((useHeuristic) ? calculate(n, end) : 0));
 
                     n.setParent(workingBox);
 
